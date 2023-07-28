cadd8db43fbb71fed79dee5a0f55fa320bc46abc
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2023-07-28 16:14:08 +0100

    Edit file

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,8 +1,11 @@
 word = "Holberton"
-print("First 3 letters: {}".format(word[:3]))
-print("Last 2 letters: {}".format(word[7:]))
-print("Middle word: {}".format(word[1:8]))
+word_first_3 = slice(0,3)
+word_last_2 = slice(-2, None)
+middle_word = slice(1,-1)
+print("First 3 letters: {}".format(word[word_first_3]))
+print("Last 2 letters: {}".format(word[word_last_2]))
+print("Middle word: {}".format(word[middle_word]))
 word = "School"
-print("First 3 letters: {}".format(word[:3]))
-print("Last 2 letters: {}".format(word[4:]))
-print("Middle word: {}".format(word[1:5]))
+print("First 3 letters: {}".format(word[word_first_3]))
+print("Last 2 letters: {}".format(word[word_last_2]))
+print("Middle word: {}".format(word[middle_word]))
