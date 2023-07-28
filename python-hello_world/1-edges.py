04a7fdfd87d1261cda4acc65048f3dfe3c023125
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2023-07-28 16:16:21 +0100

    Edit file

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -5,7 +5,3 @@ middle_word = slice(1,-1)
 print("First 3 letters: {}".format(word[word_first_3]))
 print("Last 2 letters: {}".format(word[word_last_2]))
 print("Middle word: {}".format(word[middle_word]))
-word = "School"
-print("First 3 letters: {}".format(word[word_first_3]))
-print("Last 2 letters: {}".format(word[word_last_2]))
-print("Middle word: {}".format(word[middle_word]))
