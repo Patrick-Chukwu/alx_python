37eafaa363e3848361b01b8165d104006ac2e478
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2023-07-28 17:06:13 +0100

    Edit file

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -6,12 +6,11 @@ last_digit = abs(number) % 10
 
 if last_digit > 5 and number > 0:
     print("Last digit of", number, "is", last_digit, "and is greater than 5", end=" ")
-elif last_digit > 5 and number < 0:
-    print("Last digit of", number, "is", -last_digit, "is less than 6 and not 0", end=" ")
+elif last_digit < 6 and number > 0:
+    print("Last digit of", number, "is", last_digit, "and is less than 6 and not 0", end=" ")
 elif last_digit == 0:
     print("Last digit of", number, "is", last_digit, "and is zero", end=" ")
-
-elif last_digit <= 6 and number <= 0:
-    print("Last digit of", number, "is", -last_digit, "and is less than 6 and not 0", end=" ")
+elif number < 0:
+    print("Last digit of", number, "is", -last_digit, "is less than 6 and not 0", end=" ")
 else:
-    print("Last digit of", number, "is", last_digit, "and is less than 6 and not 0", end=" ")
+    print("TypeError", end=" ")
