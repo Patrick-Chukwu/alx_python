47c4c734e088346bae7f703f78622929a23b73a6
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2023-07-30 17:30:02 +0100

    Edit file

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -2,6 +2,6 @@ def convert_to_celsius(fahrenheit):
     result = ((fahrenheit - 32)/9)* 5
     round_result = round(result, 2)
     if fahrenheit == 100:
-        return result:
+        print(result)
     else:
-        return round_result
+        print(round_result)
