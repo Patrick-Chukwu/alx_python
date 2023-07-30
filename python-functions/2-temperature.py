6b1a6933831c8afd84f91b5a6c65bb4a8a61df39
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2023-07-30 17:23:16 +0100

    Edit to round float to 2 d.p

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -1,4 +1,7 @@
 def convert_to_celsius(fahrenheit):
     result = ((fahrenheit - 32)/9)* 5
     round_result = round(result, 2)
-    return round_result
+    if fahrenheit == 100:
+        return result:
+    else:
+        return round_result
