3efcc25837b2f54f38670425a55e751c3e9b676b
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2023-07-31 21:28:35 +0100

    Edit file

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 from add_0 import add
 
-a = 1
-b = 2
-result = add(a, b)
-
-print("{} +{} = {}".format(a, b, add()))
+if __name__ = "__main__":
+    a = 1
+    b = 2
+    result = add(a, b)
+    print("{} +{} = {}".format(a, b, add()))
