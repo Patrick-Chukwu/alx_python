0c3fac2540ba2c0cc9f33f76f5621d4e7aeba415
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2023-07-31 22:14:51 +0100

    Edit file

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     length = len(argv)
     
     if (length == 1):
-        print(1 argument:)
+        print("1 argument:")
         print("1: {}".format(argv))
     
     
