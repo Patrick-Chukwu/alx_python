4b0ea130ab104c418e948c652ab9670525d0d0b2
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2023-08-02 21:37:17 +0100

    Edit file

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -22,7 +22,7 @@ import sys
 def print_arguments(argv):
     num_arguments = len(argv) - 1
     plural = 's' if num_arguments != 1 else ''
-    print(f"{num_arguments} argument{plural}:", end=' ')
+    print(f"{num_arguments} argument{plural}:",)
     
     if num_arguments == 0:
         print(".", end='\n')
