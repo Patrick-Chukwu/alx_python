ca2c048670de2178688cc3170ed42a2d23efd17d
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2023-08-02 21:27:28 +0100

    Add file

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,19 +1,37 @@
-from argv import argv
+# from argv import argv
 
-if __name__ == "__main__":
-    length = len(argv)
+# if __name__ == "__main__":
+#     length = len(argv)
     
-    if (length == 1):
-        print("1 argument:")
-        print("1: {}".format(argv))
+#     if (length == 1):
+#         print("1 argument:")
+#         print("1: {}".format(argv))
     
     
         
-    elif (length > 1):
-        print("{} arguments:".format(length))
-        for index in range(len(argv)):
-            print("{}: {}".format(index, argv[index]))
+#     elif (length > 1):
+#         print("{} arguments:".format(length))
+#         for index in range(len(argv)):
+#             print("{}: {}".format(index, argv[index]))
 
-    else: 
-        print("0 arguments.")
+#     else: 
+#         print("0 arguments.")
+
+import sys
+
+def print_arguments(argv):
+    num_arguments = len(argv) - 1
+    plural = 's' if num_arguments != 1 else ''
+    print(f"Number of argument{plural}: {num_arguments}:", end=' ')
+    
+    if num_arguments == 0:
+        print(".", end='\n')
+    else:
+        print()
+        for i in range(1, len(argv)):
+            print(f"{i}: {argv[i]}")
+
+if __name__ == "__main__":
+    print_arguments(sys.argv)
+
 
