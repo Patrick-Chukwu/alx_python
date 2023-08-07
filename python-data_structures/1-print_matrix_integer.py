183acc585c2d9ea02314b0c623f9344028b5524b
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2023-08-07 15:59:06 +0100

    Edit file

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,3 +1,12 @@
+# def print_matrix_integer(matrix=[[]]):
+#     for x, y, z in matrix:
+#         print("{:d} {:d} {:d}".format(x, y, z))
+
+
 def print_matrix_integer(matrix=[[]]):
-    for x, y, z in matrix:
-        print("{:d} {:d} {:d}".format(x, y, z))
+    for row in matrix:
+        for col_idx, element in enumerate(row):
+            if col_idx != 0:
+                print(" ", end="")
+            print("{:d}".format(element), end="")
+        print()
