62332935ac4159c9896d6e360aff70a316151eeb
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2023-08-07 15:02:51 +0100

    Edit file

--- python-data_structures/1-print_matrix_integer.py
+++ python-data_structures/1-print_matrix_integer.py
@@ -1,5 +1,3 @@
 def print_matrix_integer(matrix=[[]]):
     for x, y, z in matrix:
-        print("{}, {}, {}".format(x, y, z))
-
-print_matrix_integer([(1, 2, 3), (4, 5, 6), (7, 8, 9)])
+        print("{:d} {:d} {:d}".format(x, y, z))
