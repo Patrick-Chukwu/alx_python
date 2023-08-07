f06ee8ac3b9b06fd716a4b7646d28d7ae4f0aad5
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2023-08-07 14:22:42 +0100

    Edit file

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -1,8 +1,7 @@
 def no_c(my_string):
-
     result = []
     for character in my_string:
-       if character != 'c' and character != 'C':
+       if character != "c" and character != "C":
            result.append(character)
-    return ''.join(result)
+    return "".join(result)
 
