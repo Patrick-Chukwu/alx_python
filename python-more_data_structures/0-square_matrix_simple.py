1fa165c1283a6891e3e83abbccba8d29042c28f9
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2023-08-08 11:28:26 +0100

    Edit file

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,7 @@
 def square_matrix_simple(matrix[]):
     for row in enumerate(matrix):
         for col_index, element in enumerate(row):
-            element += 10
+            element  **= 2
             if col_index != 0:
                 print(" ", end = "")
             print("{:d}".format(element), end = "")
