4924b168907cd56431de01df6b6f81f03f4e153b
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2023-08-11 23:07:36 +0100

    Edit file

--- python-more_data_structures/1-common_elements.py
+++ python-more_data_structures/1-common_elements.py
@@ -1,2 +1,9 @@
 def update_dictionary(a_dictionary, key, value):
-    a_dictionary[key] = value
+    if a_dictionary is not None:
+        a_dictionary[key] = value
+
+def print_sorted_dictionary(my_dict):
+    if my_dict is not None:
+        keys = sorted(my_dict.keys())
+        for key in keys:
+            print(f"{key}: {my_dict[key]}")
