e0dc04d6c4850facef0f8025861481805c7e2759
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2023-08-17 13:35:10 +0100

    Edit file

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -1,6 +1,6 @@
 def update_dictionary(a_dictionary, key, value):
     a_dictionary[key] = value
-    # keys = sorted(a_dictionary.keys())
+    keys = sorted(a_dictionary.keys())
     for key in a_dictionary:
         print("{}: {} ".format(key, a_dictionary[key]))
     
