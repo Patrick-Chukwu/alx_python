ffe478e892e68756fe40c8405efcf764f8bd4b18
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2023-08-17 12:03:24 +0100

    Update file

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,24 @@
-def square_matrix_simple(matrix[]):
-    square_matrix = []
-    for row in enumerate(matrix):
-        for col_index, element in enumerate(row):
-            element  **= 2
-            if col_index != 0:
-                print(" ", end = "")
-            print("{:d}".format(element), end = "")
-        print()
-    print()
+
+def square_matrix_simple(matrix=[]):
+    # Create a new matrix to store the squared values
+    squared_matrix = []
+    
+    # Iterate through each row in the input matrix
+    for row in matrix:
+        # Create a new row for the squared values
+        squared_row = []
+        
+        # Iterate through each element in the current row
+        for element in row:
+            # Square the element and add it to the squared row
+            squared_row.append(element ** 2)
+        
+        # Add the squared row to the squared matrix
+        squared_matrix.append(squared_row)
+
+    for row in squared_matrix:
+      print(row)
+    return squared_matrix
+    # Print the squared matrix
+    
+
