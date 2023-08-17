03cd568f786244722b8d5a46448c3916797acc25
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2023-08-17 12:54:37 +0100

    Update file

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -1,2 +1,26 @@
 def update_dictionary(a_dictionary, key, value):
     a_dictionary[key] = value
+    # keys = sorted(a_dictionary.keys())
+    for key in a_dictionary:
+        print("{}: {}".format(key, a_dictionary[key]))
+
+    # return update_dictionary
+    
+
+# def print_sorted_dictionary(my_dict):
+#     """ Print sorted dictionary """
+#     keys = sorted(my_dict.keys())
+#     for k in keys:
+#         print("{}: {}".format(k, my_dict[k]))
+
+# a_dictionary = { 'language': "C", 'number': 89, 'track': "Low level" }
+# new_dict = update_dictionary(a_dictionary, 'language', "Python")
+# print_sorted_dictionary(new_dict)
+# print("--")
+# print_sorted_dictionary(a_dictionary)
+
+
+a_dictionary = {"a": "a", "b": "b" , "c": "c", "d": "d", "e": "e"} 
+# key = "p"
+# value = "P"
+update_dictionary(a_dictionary, "b", "B" )
