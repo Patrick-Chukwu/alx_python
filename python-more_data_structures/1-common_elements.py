a143bb830d487f301ed15ffd6756593ae6ddb5cb
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2023-08-17 13:55:00 +0100

    Update file

--- python-more_data_structures/1-common_elements.py
+++ python-more_data_structures/1-common_elements.py
@@ -1,9 +1,3 @@
-def update_dictionary(a_dictionary, key, value):
-    if a_dictionary is not None:
-        a_dictionary[key] = value
-
-def print_sorted_dictionary(my_dict):
-    if my_dict is not None:
-        keys = sorted(my_dict.keys())
-        for key in keys:
-            print(f"{key}: {my_dict[key]}")
+def common_elements(set_1, set_2):
+    result = set_1.intersection(set_2)
+    return result
