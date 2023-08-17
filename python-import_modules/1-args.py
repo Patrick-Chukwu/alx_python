c2fd1fa55f74548dded22475fb1c626a1472659b
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2023-08-17 14:26:50 +0100

    Update file

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,3 +1,21 @@
+# import sys
+
+# def print_arguments(argv):
+#     num_arguments = len(argv)
+#     if num_arguments == 0:
+#         print("0 argument.")
+#     elif num_arguments == 1:
+#         print("1 argument:")
+#         print(f"1: {argv[0]}")
+#     else:
+#         print(f"{num_arguments} arguments:")
+#         for i, arg in enumerate(argv, start=1):
+#             print(f"{i}: {arg}")
+
+# if __name__ == "__main__":
+#     print_arguments(sys.argv[1:])
+
+
 from argv import argv
 
 if __name__ == "__main__":
