c3bc7f921cc9a4f516dbff998a8d6ac898c79a32
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2023-08-17 14:28:15 +0100

    Update file

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,36 +1,36 @@
-# import sys
+import sys
 
-# def print_arguments(argv):
-#     num_arguments = len(argv)
-#     if num_arguments == 0:
-#         print("0 argument.")
-#     elif num_arguments == 1:
-#         print("1 argument:")
-#         print(f"1: {argv[0]}")
-#     else:
-#         print(f"{num_arguments} arguments:")
-#         for i, arg in enumerate(argv, start=1):
-#             print(f"{i}: {arg}")
-
-# if __name__ == "__main__":
-#     print_arguments(sys.argv[1:])
-
-
-from argv import argv
+def print_arguments(argv):
+    num_arguments = len(argv)
+    if num_arguments == 0:
+        print("0 argument.")
+    elif num_arguments == 1:
+        print("1 argument:")
+        print(f"1: {argv[0]}")
+    else:
+        print(f"{num_arguments} arguments:")
+        for i, arg in enumerate(argv, start=1):
+            print(f"{i}: {arg}")
 
 if __name__ == "__main__":
-    def print_arguments(argv):
-        num_arguments = len(argv) - 1
-        plural = 's' if num_arguments != 1 else ''
-        print(f"{num_arguments} argument{plural}:")
-    
-        if num_arguments == 0:
+    print_arguments(sys.argv[1:])
 
-            print()
-        else:
+
+# from argv import argv
+
+# if __name__ == "__main__":
+#     def print_arguments(argv):
+#         num_arguments = len(argv) - 1
+#         plural = 's' if num_arguments != 1 else ''
+#         print(f"{num_arguments} argument{plural}:")
+    
+#         if num_arguments == 0:
+
+#             print()
+#         else:
                      
         
-        # print()
-            for i in range(1, len(argv)):
-                print(f"{i}: {argv[i]}")
+#         # print()
+#             for i in range(1, len(argv)):
+#                 print(f"{i}: {argv[i]}")
    
