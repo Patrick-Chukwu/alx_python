874536f77274981630210f912cc78efdd3842c2a
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2023-08-18 23:11:01 +0100

    Create a class Square with method area

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,9 +1,11 @@
+
 class Square:
     """ This is a class with a private instance
     the size is the private instance
 
     """
     def __init__(self, size):
-        self.__size = size
-
-
+        self.__size = size  # Private instance attribute
+    
+    def area(self):
+        return self.__size ** 2
