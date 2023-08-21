16479d96b70abb6632aeead75d70c900b39f7db1
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2023-08-21 23:01:01 +0100

    Add conditions to the instance

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,3 +1,22 @@
 class Square:
-    def __init__(self, size):
-        self.__size = self
+    """ This is a class with a private instance
+    the size is the private instance
+    
+    
+    """
+    def __init__(self, size = 0):
+        """
+        
+        Args:
+            size (int): takes the number whose square will be found.
+        """
+        
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        
+        self.__size = size  # Private instance attribute
+
+
