7a38474769e797987310de4fdf3200ee43d0a494
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2023-08-21 22:36:07 +0100

    Create a class Square wuth private instance

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -2,10 +2,14 @@
 class Square:
     """ This is a class with a private instance
     the size is the private instance
-
+    
+    
     """
     def __init__(self, size):
+        """
+        
+        Args:
+            size (int): takes the number whose square will be found.
+        """
+        
         self.__size = size  # Private instance attribute
-    
-    def area(self):
-        return self.__size ** 2
