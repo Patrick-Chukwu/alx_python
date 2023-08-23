97f5cbe21441160053945f4b33911c8454cc953f
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2023-08-24 00:19:02 +0100

    Define classes using python inheritances

---
+++ python-inheritance/0-is_same_class.py
@@ -0,0 +1,11 @@
+"""Function to test for the type of an object"""
+
+def is_same_class(obj, a_class):
+    """
+    My main function.
+    Arguments:
+        obj
+        a_class
+
+    """
+    return type(obj) is a_class
