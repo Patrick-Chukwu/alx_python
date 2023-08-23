9fc346c4b761279afba7819e7e722b8e7b2187c7
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2023-08-24 00:24:26 +0100

    Add a function testing isinstance

---
+++ python-inheritance/1-is_kind_of_class.py
@@ -0,0 +1,11 @@
+"""Function to test whether an object is an instance of a class"""
+
+def is_kind_of_class(obj, a_class):
+    """
+    My main function.
+    Arguments:
+        obj
+        a_class
+
+    """
+    return isinstance(obj) is a_class
