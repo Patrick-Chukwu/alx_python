84579f957d86ab6bd8c36928f5d7b1c2654200b6
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2023-08-24 10:04:42 +0100

    Add a function that validates type and raise errors if otherwise

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,15 @@
+"""An empty class"""
+class BaseGeometry:
+    """My BaseGeometry class"""
+    pass
+    
+    def area(self):
+        """function Area"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """function integer_validator"""
+        if type(value) != int:
+            raise TypeError("<name> must be an integer")
+        if value <= 0:
+            raise ValueError("<name> must be greater than 0")
