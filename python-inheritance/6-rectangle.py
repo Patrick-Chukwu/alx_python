9fed04eb89488bc9be7aa581c03d35dd5d3f8336
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2023-08-24 11:07:43 +0100

    Update file to ensure Rectangle is a subclass

--- python-inheritance/6-rectangle.py
+++ python-inheritance/6-rectangle.py
@@ -24,9 +24,7 @@ class Rectangle(BaseGeometry):
             height
         """
         super()
-        self.__width = 0
-        self.__height = 0
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
         self.__width = width
         self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
