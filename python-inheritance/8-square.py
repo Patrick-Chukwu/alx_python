1ed753e9f8c50fc691a1717a6189d68052c9a4f6
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2023-08-24 11:36:56 +0100

    Add square subclass

---
+++ python-inheritance/8-square.py
@@ -0,0 +1,51 @@
+"""An empty class"""
+class BaseGeometry:
+    """My BaseGeometry class"""
+    pass
+    
+    def area(self):
+        """function Area"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """function integer_validator"""
+        if type(value) != int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+        
+class Rectangle(BaseGeometry):
+    """A Rectangle subclass"""
+    def __init__(self, width, height):
+        """
+        Define the init method and inherits from the super.
+        Arguments:
+            width
+            height
+        """
+        super()
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+    def area(self):
+        """Area function"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """A function str"""
+        return f"[Rectangle] {self.__width}/{self.__height}"
+class Square(Rectangle):
+    """A subclass for Square"""
+    def __init__(self, size):
+        """A init function.
+        Arguments:
+            size
+        """
+        self.__size = size
+        self.integer_validator("size", size)
+
+    def area(self):
+        """An area function"""
+        return self.__size ** 2
