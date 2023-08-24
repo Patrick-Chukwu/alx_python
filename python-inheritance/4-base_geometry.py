f64552ab276a3a50e038197fe60a58f28cb7bf10
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2023-08-24 09:56:03 +0100

    Create function to raise exception

---
+++ python-inheritance/4-base_geometry.py
@@ -0,0 +1,8 @@
+"""An empty class"""
+class BaseGeometry:
+    """My BaseGeometry class"""
+    pass
+    
+    def area(self):
+        """function Area"""
+        raise Exception("area() is not implemented")
