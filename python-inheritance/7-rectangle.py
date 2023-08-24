bb3aa270cdfb5d098fc55b2875a78da4d7e07515
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2023-08-24 11:17:34 +0100

    Add file

---
+++ python-inheritance/7-rectangle.py
@@ -0,0 +1,36 @@
+"""An empty class"""
+class BaseGeometry:
+    """My BaseGeometry class"""
+    pass
+    
+    def area(self):
+        """function Area"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """function integer_validator"""
+        if type(value) != int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+        
+class Rectangle(BaseGeometry):
+    """A Rectangle subclass"""
+    def __init__(self, width, height):
+        """
+        Define the init method and inherits from the super.
+        Arguments:
+            width
+            height
+        """
+        super()
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+    def area(self):
+        """function Area"""
+        str = f"[Rectangle] {width}/{height}"
+        result = width * height
+        return str result
