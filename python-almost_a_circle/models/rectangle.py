09844cc47de889b1c5c778bcf07620cd8cd1ae0b
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2023-08-26 21:51:07 +0100

    Update the file with a for loop A for loop to cater for x and y values

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -87,9 +87,9 @@ class Rectangle(Base):
     def display(self):
         """A public method that prints in stdout"""
         for _ in range(self.__y):
-            print("$")
+            print(" ")
         for _ in range(self.__height):
-            print("#" * self.__width + "$" * self.__x)
+            print(" " * self.__x + "#" * self.__width )
 
   
     def __str__(self):
