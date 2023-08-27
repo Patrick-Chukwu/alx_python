68fe2ffe7321104b5b66dabee8908d2da216b927
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2023-08-27 19:16:27 +0100

    Create a file to request from a URL

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,10 @@
+"""Importing requests and using it to print a response
+    """
+import requests
+
+
+response = requests.get('https://alu-intranet.hbtn.io/status')
+
+print("Body response:")
+print("\t- type:", type(response.text))
+print("\t- content:", response.text)
