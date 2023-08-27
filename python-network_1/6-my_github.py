517b0a6b13e5de964ab080743c1f8907329b9a34
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2023-08-27 20:51:44 +0100

    Add file

--- python-network_1/6-my_github.py
+++ python-network_1/6-my_github.py
@@ -4,7 +4,7 @@ import requests
 import sys
 
 if len(sys.argv) < 3:
-    print("Usage: python script.py <username> <personal_access_token>")
+    print("Usage: python script.py Patrick-Chukwu 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")
     sys.exit(1)
 
 username = sys.argv[1]
@@ -18,3 +18,24 @@ if response.status_code == 200:
     print("Your GitHub ID:", user_data["id"])
 else:
     print("Error:", response.text)
+
+
+
+# import requests
+# import sys
+
+# if len(sys.argv) < 3:
+#     print("Usage: python script.py <username> {<personal_access_token>")
+#     sys.exit(1)
+
+# username = sys.argv[1]
+# token = sys.argv[2]
+# url = "https://api.github.com/user"
+
+# response = requests.get(url, auth=(username, token))
+
+# if response.status_code == 200:
+#     user_data = response.json()
+#     print("Your GitHub ID:", user_data["id"])
+# else:
+#     print("Error:", response.text)
