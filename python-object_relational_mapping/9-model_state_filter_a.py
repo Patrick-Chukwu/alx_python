e99438555e5fc83052e4e6ef3a0f4eb5b96d857d
Chukwu Patrick Ifeanyi <chukwupatrickify@gmail.com>
2023-09-05 19:35:05 +0100

    Corrected the documentation quote

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,7 +1,7 @@
-"""
+'''
 a script that lists all State objects
 that contain the letter a from the database
-"""
+'''
 if __name__ == "__main__":
     from model_state import State, Base
     from sqlalchemy.orm import sessionmaker
